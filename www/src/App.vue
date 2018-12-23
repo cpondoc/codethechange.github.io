9a2fc74e006714b032cb26c3e5499a1900a20d9e
DrewGregory <djgregny@gmail.com>
2018-12-23 12:39:06 -0500

    added bootstrap

--- www/src/App.vue
+++ www/src/App.vue
@@ -7,6 +7,13 @@
     <router-view/>
   </div>
 </template>
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'App'
+}
+</script>
 
 <style>
 #app {
--- www/src/components/HelloWorld.vue
+++ www/src/components/HelloWorld.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="hello">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+  With Bootstrap!
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>

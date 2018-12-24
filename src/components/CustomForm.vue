3501b7273e2619f5db7cbb62cad5e2a1099163d4
DrewGregory <djgregny@gmail.com>
2018-12-24 01:59:18 -0500

    theming

--- src/components/CustomForm.vue
+++ src/components/CustomForm.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        <p> we;re in custom form with special property: {{ content }}</p>
-        <button class="btn btn-primary" @click="runMethod" ></button>
+    <div class="content">
+        <div class="row">
+            <div id="leftCol" class="col-4">
+                <p> we;re in custom form with special property: {{ content }}</p>
+            </div>
+            <div id="rightCol" class="col-8">
+                <button class="btn btn-accent" @click="runMethod" >Here's some text</button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -15,3 +21,17 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+@import '../theme.scss';
+p {
+    color: map-get($theme-colors, primary)
+}
+
+#leftCol {
+    background-color: #3212aa
+}
+
+#rightCol {
+    background-color: #ab24cc
+}
+</style>

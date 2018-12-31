3bfd9095d49bbda3e1c3d4f9a534d16ac1a1f746
DrewGregory <djgregny@gmail.com>
2018-12-31 17:05:37 -0500

    footer

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,24 @@
       </div>
     </div>
     <router-view/>
+    <div id="footer">
+      <div class="link-list">
+        <p class="link-header">for students</p>
+        <hr class="link-line" />
+        <router-link class="small-link-text" to="/people">people</router-link>
+        <br /> <br />
+        <router-link class="small-link-text" to="/projects">projects</router-link>
+        <br /> <br />
+        <router-link class="small-link-text" to="/join" id="join-link">join</router-link>
+        <br /> <br />
+      </div>
+      <div class="link-list">
+        <p class="link-header">for organizations</p>
+        <hr class="link-line" />
+        <router-link class="small-link-text" to="/partner">partner</router-link>
+        <br /> <br />
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -25,12 +43,53 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #ffffff;
+  background-color: $main-dark;
+}
+
+.link-line {
+  background-color:$accent-light;
+  height: 2px;
+}
+
+.small-link-text {
+  font-family: Comfortaa;
+  font-size: 24px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #ffffff;
+}
+
+#footer {
+  width: 100vw;
+  padding-top: 50px;
+  display: flex;
+  background-color: $main-dark;
+}
+
+.link-list {
+  margin-left: 85px;
+}
+
+.link-header {
+  font-family: Comfortaa;
+  font-size: 24px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
   text-align: center;
   color: #ffffff;
-  background-color: #3b3b3b;
 }
 
 .nav-container {
+  background-color: $main-dark;
   padding: 50px;
   width: 100vw;
   display: flex;

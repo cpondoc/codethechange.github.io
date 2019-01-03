53101087546ef6cdc44b1af5da6cf6bf362fadef
DrewGregory <djgregny@gmail.com>
2019-01-02 21:41:48 -0500

    basic panel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div id="text">
+      <br />
+      <h1 id="hook">Code from day one, with impact.</h1>
+      <hr id="line" />
+      <p id="subtitle">Welcome to a passionate club of engineers and makers.</p>
+      <br />
+      <br />
+      <router-link id="view-text" to="/projects">View our work</router-link>
+      <br />
+    </div>
+    <img id="stanford" src="@/assets/img/stanford@2x.png" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style scoped lang="scss">
+@import '../theme.scss';
+.container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+#text {
+  width: 500px;
+}
+
+#line {
+  width: 125px;
+  height: 2px;
+  background-color: $home-accent;
+  margin-left: 0px;
+}
+
+#subtitle {
+  font-family: Comfortaa;
+  font-size: 30px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #ffffff;
+}
+
+#hook {
+  font-family: Comfortaa;
+  font-size: 60px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #ffffff;
+}
+
+#view-text {
+  font-family: Comfortaa;
+  font-size: 32px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: $home-accent;
+}
+
+#stanford {
+  width: 30em;
+  height: 30em;
+}
+</style>

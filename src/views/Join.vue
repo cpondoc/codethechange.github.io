53aa7ab5329971d2b99a652fef5b04d99da33de1
samjgorman <samjgorman@gmail.com>
2019-01-03 18:57:16 -0800

    html-join

---
+++ src/views/Join.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class = "header-container">
+        <div class = "header-group">
+            <h1 class = "header-title">Change the world and build projects with us.</h1>
+                <div class = "cta-button"> 
+                    <h3> Apply </h3>
+                     <div id = "button-arrow">
+                </div>
+                    </div>
+        <div>
+    </div>
+
+    <div class = "container-right">
+        <div class = "cr-text">
+            <h2 id = "code-h2">Make code your superpower…</h2>
+            <p  id = "code-p"> Technical leadership and support from Stanford student volunteers dedicating 5 to 10 hours per week on the project free of cost. </p>
+        </div>
+        <div class = "cr-img">  </div>
+    </div>
+
+     <div class = "container-left">
+        <div class = "cl-img">  </div>
+        <div class = "cl-text">
+            <h2>unless your superpower is design.</h2>
+            <p>Technical leadership and support from Stanford student volunteers dedicating 5 to 10 hours per week on the project free of cost. </p>
+        </div>
+    </div>
+
+    <div class = "container-right">
+        <div class = "cr-text">
+            <h2 id = "family-h2" >You’re joining a family</h2>
+            <p id = "family-p">We built a project matching platform that boosted engagement by 60%.   We’re Code the Change, a club at Stanford University that builds useful software for high-impact nonprofits.  </p>
+        </div>
+        <div class = "cr-img">  </div>
+    </div>
+
+    <div class = "cta">
+        <h1> Ready to jump in?  Apply today. </h1>
+        <div class = "cta-button"> 
+                    <h3> Apply </h3>
+                     <div id = "button-arrow">
+                    </div>
+        </div>
+
+        </div>
+
+</template>
+
+
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>

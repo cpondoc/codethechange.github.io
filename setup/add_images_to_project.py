5ed9e238ab58f69f60d744ab00cdc4922e0d74c6
Drew Gregory <djgregny@gmail.com>
2019-09-18 22:10:53 -0700

    easy add image to project

--- setup/add_images_to_project.py
+++ setup/add_images_to_project.py
@@ -10,6 +10,7 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 import argparse
 import shutil
+import load_write_projects
 parser = argparse.ArgumentParser(description='Add images to projects.json')
 parser.add_argument('project_name', type=str, help="project name field as specified in projects.json")
 parser.add_argument('image_file', type=str, help='path to the image file')
@@ -17,4 +18,12 @@ parser.add_argument('caption', type=str, help='caption to the image')
 args = parser.parse_args()
 dirpath = os.getcwd()
 shutil.move(args.image_file, dirpath + '/../public/assets/')
-
+projects = load_write_projects.load_projects()
+project = load_write_projects.get_project(projects, args.project_name)
+if 'assets' not in project:
+    project['assets'] = []
+project['assets'].append({
+    'caption': args.caption,
+    'src': './assets/{}'.format(args.image_file.split('/')[-1]),
+})
+load_write_projects.update_projects(projects)
